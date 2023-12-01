71b67301194256d00ff6c55dbc7954d97fefe28e
Zack Tsai <71244072+tchjackin1125@users.noreply.github.com>
2023-12-01 12:57:54 -0600

    Create 20231201.py

---
+++ 20231201.py
@@ -0,0 +1,2 @@
+yes, currently  partially resets after each download, the uploads stay the same, it was not a problem a few hours ago though 
+If it cannot easily be fixed or debugged, a solution maybe to just create a download all button
